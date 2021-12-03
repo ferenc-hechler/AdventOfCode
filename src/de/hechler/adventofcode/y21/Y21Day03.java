0f9fcd886d655d1fedff9ba1038619fbd35f1118
Ferenc Hechler <ferenc.hechler@t-systems.com>
2021-12-03 10:52:04 +0100

    day 3 part 2.

--- src/de/hechler/adventofcode/y21/Y21Day03.java
+++ src/de/hechler/adventofcode/y21/Y21Day03.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * see: https://adventofcode.com/2021/day/1
@@ -67,9 +68,75 @@ public class Y21Day03 {
 		}
 	}
 
+	public static void mainPart2() throws FileNotFoundException {
+		try (Scanner scanner = new Scanner(new File("input/y21/day03.txt"))) {
+			List<String> scanResult = new ArrayList<>();
+			int bits = 0;
+			int sumZeros[] = null;
+			int sumOnes[] = null;
+			while (scanner.hasNext()) {
+				String line = scanner.nextLine().trim();
+				if (line.isEmpty()) {
+					continue;
+				}
+				if (!line.matches(INPUT_RX)) {
+					throw new RuntimeException("invalid input line '"+line+"', not matching RX '"+INPUT_RX+"'");
+				}
+				if (bits == 0) {
+					bits = line.length();
+					sumZeros = new int[bits];
+					sumOnes = new int[bits];
+				}
+				if (line.length() != bits) {
+					throw new RuntimeException("invalid input line '"+line+"', expected length '"+bits+"'");
+				}
+				String binNum = line.replaceFirst(INPUT_RX, "$1");
+				for (int i=0; i<bits; i++) {
+					sumZeros[i] += (binNum.charAt(i) == '0' ? 1 : 0); 
+					sumOnes[i] += (binNum.charAt(i) == '1' ? 1 : 0); 
+				}
+				scanResult.add(binNum);
+			}
+			System.out.println("#scans: "+scanResult.size());
+			String oxygeneGeneratorRating = searchOxygeneRating(scanResult, 0, sumZeros, sumOnes);
+			System.out.println("oxygeneGeneratorRating: "+oxygeneGeneratorRating);
+			String co2ScrubberRating = searchCO2ScrubberRating(scanResult, 0, sumZeros, sumOnes);
+			System.out.println("co2ScrubberRating: "+co2ScrubberRating);
+			int oxygene = Integer.parseInt(oxygeneGeneratorRating, 2);
+			int co2 = Integer.parseInt(co2ScrubberRating, 2);
+			System.out.println(oxygene + " x " + co2 + " = " + (oxygene * co2));
+		}
+	}
+
+	
+	private static String searchOxygeneRating(List<String> filteredResult, int bitPos, int[] sumZeros, int[] sumOnes) {
+		int[] sums = calcSums(filteredResult, bitPos);
+		char mostCommonChar = (sums[0] > sums[1]) ? '0' : '1';
+		List<String> filterMostCommon = filteredResult.stream().filter(bitNum -> bitNum.charAt(bitPos)==mostCommonChar).collect(Collectors.toList());
+		if (filterMostCommon.size()==1) {
+			return filterMostCommon.get(0);
+		}
+		return searchOxygeneRating(filterMostCommon, bitPos+1, sumZeros, sumOnes);
+	}
+
+	private static int[] calcSums(List<String> scans, int bitPos) {
+		final int[] result = {0,0};
+		scans.forEach(scan -> result[scan.charAt(bitPos)=='0'?0:1]++);
+		return result;
+	}
+
+	private static String searchCO2ScrubberRating(List<String> filteredResult, int bitPos, int[] sumZeros, int[] sumOnes) {
+		int[] sums = calcSums(filteredResult, bitPos);
+		char mostCommonChar = (sums[0] <= sums[1]) ? '0' : '1';
+		List<String> filterMostCommon = filteredResult.stream().filter(bitNum -> bitNum.charAt(bitPos)==mostCommonChar).collect(Collectors.toList());
+		if (filterMostCommon.size()==1) {
+			return filterMostCommon.get(0);
+		}
+		return searchCO2ScrubberRating(filterMostCommon, bitPos+1, sumZeros, sumOnes);
+	}
 
 	public static void main(String[] args) throws FileNotFoundException {
-		mainPart1();
+		mainPart2();
 	}
 
 	
