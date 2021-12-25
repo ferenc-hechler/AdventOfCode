f07286d930b4a15ae34a99e04706d454dccd506c
feri <github@fh.anderemails.de>
2021-12-25 02:57:37 +0100

    optimized search code.

--- src/de/hechler/adventofcode/y21/Y21Day24SearchCode.java
+++ src/de/hechler/adventofcode/y21/Y21Day24SearchCode.java
@@ -1,334 +1,69 @@
 package de.hechler.adventofcode.y21;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * see: https://adventofcode.com/2021/day/22
  *
  */
 public class Y21Day24SearchCode {
 
-	public static int[] input = null;
-
-	public static int inp(int idx) {
-		return input[idx];
-	}
-
-	public static int add(int a, int b) {
-		return a + b;
-	}
-
-	public static int mul(int a, int b) {
-		return a * b;
-	}
-
-	public static int mod(int a, int b) {
-		return a % b;
-	}
-
-	public static int div(int a, int b) {
-		return a / b;
-	}
-
-	public static int eql(int a, int b) {
-		return a == b ? 1 : 0;
-	}
-
-	public static boolean aluSimulator() {
-		int w = 0;
-		int x = 0;
-		int y = 0;
-		int z = 0;
-		int i = 0;
-		w = inp(i++);
-		x = mul(x, 0);
-		x = add(x, z);
-		x = mod(x, 26);
-		z = div(z, 1);
-		x = add(x, 14);
-		x = eql(x, w);
-		x = eql(x, 0);
-		y = mul(y, 0);
-		y = add(y, 25);
-		y = mul(y, x);
-		y = add(y, 1);
-		z = mul(z, y);
-		y = mul(y, 0);
-		y = add(y, w);
-		y = add(y, 12);
-		y = mul(y, x);
-		z = add(z, y);
-		result(w,x,y,z);
-		w = inp(i++);
-		x = mul(x, 0);
-		x = add(x, z);
-		x = mod(x, 26);
-		z = div(z, 1);
-		x = add(x, 11);
-		x = eql(x, w);
-		x = eql(x, 0);
-		y = mul(y, 0);
-		y = add(y, 25);
-		y = mul(y, x);
-		y = add(y, 1);
-		z = mul(z, y);
-		y = mul(y, 0);
-		y = add(y, w);
-		y = add(y, 8);
-		y = mul(y, x);
-		z = add(z, y);
-		result(w,x,y,z);
-		w = inp(i++);
-		x = mul(x, 0);
-		x = add(x, z);
-		x = mod(x, 26);
-		z = div(z, 1);
-		x = add(x, 11);
-		x = eql(x, w);
-		x = eql(x, 0);
-		y = mul(y, 0);
-		y = add(y, 25);
-		y = mul(y, x);
-		y = add(y, 1);
-		z = mul(z, y);
-		y = mul(y, 0);
-		y = add(y, w);
-		y = add(y, 7);
-		y = mul(y, x);
-		z = add(z, y);
-		result(w,x,y,z);
-		w = inp(i++);
-		x = mul(x, 0);
-		x = add(x, z);
-		x = mod(x, 26);
-		z = div(z, 1);
-		x = add(x, 14);
-		x = eql(x, w);
-		x = eql(x, 0);
-		y = mul(y, 0);
-		y = add(y, 25);
-		y = mul(y, x);
-		y = add(y, 1);
-		z = mul(z, y);
-		y = mul(y, 0);
-		y = add(y, w);
-		y = add(y, 4);
-		y = mul(y, x);
-		z = add(z, y);
-		result(w,x,y,z);
-		w = inp(i++);
-		x = mul(x, 0);
-		x = add(x, z);
-		x = mod(x, 26);
-		z = div(z, 26);
-		x = add(x, -11);
-		x = eql(x, w);
-		x = eql(x, 0);
-		y = mul(y, 0);
-		y = add(y, 25);
-		y = mul(y, x);
-		y = add(y, 1);
-		z = mul(z, y);
-		y = mul(y, 0);
-		y = add(y, w);
-		y = add(y, 4);
-		y = mul(y, x);
-		z = add(z, y);
-		result(w,x,y,z);
-		w = inp(i++);
-		x = mul(x, 0);
-		x = add(x, z);
-		x = mod(x, 26);
-		z = div(z, 1);
-		x = add(x, 12);
-		x = eql(x, w);
-		x = eql(x, 0);
-		y = mul(y, 0);
-		y = add(y, 25);
-		y = mul(y, x);
-		y = add(y, 1);
-		z = mul(z, y);
-		y = mul(y, 0);
-		y = add(y, w);
-		y = add(y, 1);
-		y = mul(y, x);
-		z = add(z, y);
-		result(w,x,y,z);
-		w = inp(i++);
-		x = mul(x, 0);
-		x = add(x, z);
-		x = mod(x, 26);
-		z = div(z, 26);
-		x = add(x, -1);
-		x = eql(x, w);
-		x = eql(x, 0);
-		y = mul(y, 0);
-		y = add(y, 25);
-		y = mul(y, x);
-		y = add(y, 1);
-		z = mul(z, y);
-		y = mul(y, 0);
-		y = add(y, w);
-		y = add(y, 10);
-		y = mul(y, x);
-		z = add(z, y);
-		result(w,x,y,z);
-		w = inp(i++);
-		x = mul(x, 0);
-		x = add(x, z);
-		x = mod(x, 26);
-		z = div(z, 1);
-		x = add(x, 10);
-		x = eql(x, w);
-		x = eql(x, 0);
-		y = mul(y, 0);
-		y = add(y, 25);
-		y = mul(y, x);
-		y = add(y, 1);
-		z = mul(z, y);
-		y = mul(y, 0);
-		y = add(y, w);
-		y = add(y, 8);
-		y = mul(y, x);
-		z = add(z, y);
-		result(w,x,y,z);
-		w = inp(i++);
-		x = mul(x, 0);
-		x = add(x, z);
-		x = mod(x, 26);
-		z = div(z, 26);
-		x = add(x, -3);
-		x = eql(x, w);
-		x = eql(x, 0);
-		y = mul(y, 0);
-		y = add(y, 25);
-		y = mul(y, x);
-		y = add(y, 1);
-		z = mul(z, y);
-		y = mul(y, 0);
-		y = add(y, w);
-		y = add(y, 12);
-		y = mul(y, x);
-		z = add(z, y);
-		result(w,x,y,z);
-		w = inp(i++);
-		x = mul(x, 0);
-		x = add(x, z);
-		x = mod(x, 26);
-		z = div(z, 26);
-		x = add(x, -4);
-		x = eql(x, w);
-		x = eql(x, 0);
-		y = mul(y, 0);
-		y = add(y, 25);
-		y = mul(y, x);
-		y = add(y, 1);
-		z = mul(z, y);
-		y = mul(y, 0);
-		y = add(y, w);
-		y = add(y, 10);
-		y = mul(y, x);
-		z = add(z, y);
-		result(w,x,y,z);
-		w = inp(i++);
-		x = mul(x, 0);
-		x = add(x, z);
-		x = mod(x, 26);
-		z = div(z, 26);
-		x = add(x, -13);
-		x = eql(x, w);
-		x = eql(x, 0);
-		y = mul(y, 0);
-		y = add(y, 25);
-		y = mul(y, x);
-		y = add(y, 1);
-		z = mul(z, y);
-		y = mul(y, 0);
-		y = add(y, w);
-		y = add(y, 15);
-		y = mul(y, x);
-		z = add(z, y);
-		result(w,x,y,z);
-		w = inp(i++);
-		x = mul(x, 0);
-		x = add(x, z);
-		x = mod(x, 26);
-		z = div(z, 26);
-		x = add(x, -8);
-		x = eql(x, w);
-		x = eql(x, 0);
-		y = mul(y, 0);
-		y = add(y, 25);
-		y = mul(y, x);
-		y = add(y, 1);
-		z = mul(z, y);
-		y = mul(y, 0);
-		y = add(y, w);
-		y = add(y, 4);
-		y = mul(y, x);
-		z = add(z, y);
-		result(w,x,y,z);
-		w = inp(i++);
-		x = mul(x, 0);
-		x = add(x, z);
-		x = mod(x, 26);
-		z = div(z, 1);
-		x = add(x, 13);
-		x = eql(x, w);
-		x = eql(x, 0);
-		y = mul(y, 0);
-		y = add(y, 25);
-		y = mul(y, x);
-		y = add(y, 1);
-		z = mul(z, y);
-		y = mul(y, 0);
-		y = add(y, w);
-		y = add(y, 10);
-		y = mul(y, x);
-		z = add(z, y);
-		result(w,x,y,z);
-		w = inp(i++);
-		x = mul(x, 0);
-		x = add(x, z);
-		x = mod(x, 26);
-		z = div(z, 26);
-		x = add(x, -11);
-		x = eql(x, w);
-		x = eql(x, 0);
-		y = mul(y, 0);
-		y = add(y, 25);
-		y = mul(y, x);
-		y = add(y, 1);
-		z = mul(z, y);
-		y = mul(y, 0);
-		y = add(y, w);
-		y = add(y, 9);
-		y = mul(y, x);
-		z = add(z, y);
-		return z == 0;
-	}
-
-	public static void result(int w, int x, int y, int z) {
-		System.out.println("w=" + w + ", x=" + x + ", y=" + y + ", z=" + z);
-	}
-
-	public static void run() {
-		input = new int[] {2, 6, 1, 8, 1, 1, 1, 1, 6, 4, 1, 6, 2, 1};
-		int cnt = 0;
-		boolean valid = aluSimulator();
-		System.out.println(Utils.toList(input));
-		System.out.println(valid);
-	}
-
-	private static void decrementInput() {
-		for (int pos=13; pos>=0; pos--) {
-			if (--input[pos] > 0) {
-				return;
-			}
-			input[pos] = 9;
+	private final static int[][] ARGS_LIST = { 
+			{ 1, 14, 12 }, 
+			{ 1, 11, 8 }, 
+			{ 1, 11, 7 }, 
+			{ 1, 14, 4 }, 
+			{ 26, -11, 4 },
+			{ 1, 12, 1 }, 
+			{ 26, -1, 10 }, 
+			{ 1, 10, 8 }, 
+			{ 26, -3, 12 }, 
+			{ 26, -4, 10 }, 
+			{ 26, -13, 15 }, 
+			{ 26, -8, 4 },
+			{ 1, 13, 10 }, 
+			{ 26, -11, 9 } 
 		};
+
+
+	public static List<Integer> monad(int step, int z) {
+		if (step == 14) {
+			if (z==0) {
+				return new ArrayList<>();
+			}
+			return null;
+		}
+		int[] args = ARGS_LIST[step];
+		int m = (z%26)+args[1];
+		if ((m>=1) && (m<=9)) {
+			z=(z/args[0]);
+			List<Integer> result = monad(step+1, z);
+			if (result != null) {
+				result.add(0, m);
+			}
+			return result;
+		}
+		for (int w=9; w>=1; w--) {
+			if (step <= 2) {
+				System.out.println("   ".substring(0, step)+w);
+			}
+			int y = w+args[2];
+			int z2=(z/args[0])*26+y;
+			List<Integer> result = monad(step+1, z2);
+			if (result != null) {
+				result.add(0, w);
+				return result;
+			}
+		}
+		return null;
 	}
+
+
 
 	public static void main(String[] args) {
-		run();
+		List<Integer> result = monad(0, 0);
+		System.out.println(result);
 	}
 
 }
